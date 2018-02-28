0d46ff72cf01bda52e386dd9fb256237a8a5cfbc
Omochao <12854316+Omochao@users.noreply.github.com>
2018-02-28 09:05:44 -0500

    why the fuck did this happen

--- Ticketron.java
+++ Ticketron.java
@@ -2,25 +2,34 @@
  * @author JM
  * @version 28 FEB 2018
  */
-public class Ticketron
-{
-    MenuItem mocha              = new MenuItem("Monty's Mocha", 4.00);
-    MenuItem icedCoffee         = new MenuItem("Iced Coffee", 3.00);
-    MenuItem greenTeaLemonade   = new MenuItem("Green Tea Lemonade", 3.00);
-    MenuItem mixedBerrySmoothie = new MenuItem("Mixed Berry Smoothie", 4.00);
-    MenuItem mediumRoast        = new MenuItem("Medium Roast", 2.00);
-    MenuItem yogurtParfait      = new MenuItem("Yogurt Parfait", 3.00);
-    MenuItem hotChocolate       = new MenuItem("Hot Chocolate", 2.00);
-    MenuItem icyBlendedCoffee   = new MenuItem("Icy Blended Pawffee", 4.00);
-    MenuItem icedLemonade       = new MenuItem("Iced Lemonade", 2.00);
-    MenuItem latte              = new MenuItem("Latte", 3.00);
-    MenuItem greenTea           = new MenuItem("Green Tea", 2.00);
-    MenuItem espresso           = new MenuItem("Espresso", 1.00);
-    MenuItem orangeJuice        = new MenuItem("Orange Juice", 2.00);
-    MenuItem nutellaMilkshake   = new MenuItem("Nutella Milkshake", 4.00);
-    MenuItem water              = new MenuItem("Water Bottle", 1.50);
-    MenuItem croissant          = new MenuItem("Croissandwich", 5.00);
-    MenuItem cereal             = new MenuItem("Cereal Bowl", 2.00);
-    MenuItem nutellaCroissant   = new MenuItem("Nutella Croissant", 5.00);
-    MenuItem bagel              = new MenuItem("Bagel", 2.00);
+import java.util.Map;
+import java.util.HashMap;
+
+
+public class Ticketron {
+	public static void main(String[] args) {
+		Map itemlist;
+		itemlist = new HashMap<String, Double>();
+		
+	    itemlist.put("Montys Mocha", 4.00);
+	    itemlist.put("Iced Coffee", 3.00);
+	    itemlist.put("Green Tea Lemonade", 3.00);
+	    itemlist.put("Mixed Berry Smoothie", 4.00);
+	    itemlist.put("Medium Roast", 2.00);
+	    itemlist.put("Yogurt Parfait", 3.00);
+	    itemlist.put("Hot Chocolate", 2.00);
+	    itemlist.put("Icy Blended Pawffee", 4.00);
+	    itemlist.put("Iced Lemonade", 2.00);
+	    itemlist.put("Latte", 3.00);
+	    itemlist.put("Green Tea", 2.00);
+	    itemlist.put("Espresso", 1.00);
+	    itemlist.put("Orange Juice", 2.00);
+	    itemlist.put("Nutella Milkshake", 4.00);
+	    itemlist.put("Water Bottle", 1.50);
+	    itemlist.put("Croissandwich", 5.00);
+	    itemlist.put("Cereal Bowl", 2.00);
+	    itemlist.put("Nutella Croissant", 5.00);
+	    itemlist.put("Bagel", 2.00);
+	}
+    
 }

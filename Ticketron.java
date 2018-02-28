40812e2be01e71340895fb367e5f03bf728f9abf
Omochao <12854316+Omochao@users.noreply.github.com>
2018-02-28 09:09:53 -0500

    Update Ticketron.java

--- Ticketron.java
+++ Ticketron.java
@@ -10,26 +10,26 @@ public class Ticketron {
 	public static void main(String[] args) {
 		Map itemlist;
 		itemlist = new HashMap<String, Double>();
-		
-	    itemlist.put("Montys Mocha", 4.00);
-	    itemlist.put("Iced Coffee", 3.00);
-	    itemlist.put("Green Tea Lemonade", 3.00);
-	    itemlist.put("Mixed Berry Smoothie", 4.00);
-	    itemlist.put("Medium Roast", 2.00);
-	    itemlist.put("Yogurt Parfait", 3.00);
-	    itemlist.put("Hot Chocolate", 2.00);
-	    itemlist.put("Icy Blended Pawffee", 4.00);
-	    itemlist.put("Iced Lemonade", 2.00);
-	    itemlist.put("Latte", 3.00);
-	    itemlist.put("Green Tea", 2.00);
-	    itemlist.put("Espresso", 1.00);
-	    itemlist.put("Orange Juice", 2.00);
-	    itemlist.put("Nutella Milkshake", 4.00);
-	    itemlist.put("Water Bottle", 1.50);
-	    itemlist.put("Croissandwich", 5.00);
-	    itemlist.put("Cereal Bowl", 2.00);
-	    itemlist.put("Nutella Croissant", 5.00);
-	    itemlist.put("Bagel", 2.00);
+
+		itemlist.put("Montys Mocha", 4.00);
+		itemlist.put("Iced Coffee", 3.00);
+		itemlist.put("Green Tea Lemonade", 3.00);
+		itemlist.put("Mixed Berry Smoothie", 4.00);
+		itemlist.put("Medium Roast", 2.00);
+		itemlist.put("Yogurt Parfait", 3.00);
+		itemlist.put("Hot Chocolate", 2.00);
+		itemlist.put("Icy Blended Pawffee", 4.00);
+		itemlist.put("Iced Lemonade", 2.00);
+		itemlist.put("Latte", 3.00);
+		itemlist.put("Green Tea", 2.00);
+		itemlist.put("Espresso", 1.00);
+		itemlist.put("Orange Juice", 2.00);
+		itemlist.put("Nutella Milkshake", 4.00);
+		itemlist.put("Water Bottle", 1.50);
+		itemlist.put("Croissandwich", 5.00);
+		itemlist.put("Cereal Bowl", 2.00);
+		itemlist.put("Nutella Croissant", 5.00);
+		itemlist.put("Bagel", 2.00);
 	}
     
 }
